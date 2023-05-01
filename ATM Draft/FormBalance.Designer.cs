86609ad333e0123b3852237de538cb0fcfc2bd52
John Patrick <furaquejp11@gmail.com>
2023-05-01 23:28:39 +0800

    Added Check Balance.

--- ATM Draft/FormBalance.Designer.cs
+++ ATM Draft/FormBalance.Designer.cs
@@ -31,42 +31,126 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblCurrent = new System.Windows.Forms.Label();
+            this.lblAvailable = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(110, 119);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(340, 37);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(732, 25);
+            this.label1.Size = new System.Drawing.Size(196, 25);
             this.label1.TabIndex = 3;
-            this.label1.Text = "PLEASE PRESS THE PROPER KEY FOR YOUR DESIRED TRANSACTION";
+            this.label1.Text = "YOUR BALANCE:";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(419, 224);
+            this.button1.Location = new System.Drawing.Point(670, 366);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 84);
+            this.button1.Size = new System.Drawing.Size(137, 41);
             this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
+            this.button1.Text = "YES";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(440, 442);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(297, 103);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(51, 20);
             this.label2.TabIndex = 5;
             this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(129, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "ACCOUNT NO:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(165, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "CURRENT:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(155, 203);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(126, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "AVAILABLE:";
+            // 
+            // lblCurrent
+            // 
+            this.lblCurrent.AutoSize = true;
+            this.lblCurrent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrent.Location = new System.Drawing.Point(297, 148);
+            this.lblCurrent.Name = "lblCurrent";
+            this.lblCurrent.Size = new System.Drawing.Size(51, 20);
+            this.lblCurrent.TabIndex = 9;
+            this.lblCurrent.Text = "label6";
+            // 
+            // lblAvailable
+            // 
+            this.lblAvailable.AutoSize = true;
+            this.lblAvailable.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAvailable.Location = new System.Drawing.Point(297, 203);
+            this.lblAvailable.Name = "lblAvailable";
+            this.lblAvailable.Size = new System.Drawing.Size(51, 20);
+            this.lblAvailable.TabIndex = 10;
+            this.lblAvailable.Text = "label7";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(184, 275);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(503, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "WOULD YOU LIKE ANOTHER TRANSACTION?";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(670, 430);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 41);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "NO";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // FormBalance
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(870, 517);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblAvailable);
+            this.Controls.Add(this.lblCurrent);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
@@ -84,5 +168,12 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblCurrent;
+        private System.Windows.Forms.Label lblAvailable;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
     }
 }
--- ATM Draft/FormBalance.cs
+++ ATM Draft/FormBalance.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,9 +21,10 @@ namespace ATM_Draft
             this.id = id;
         }
 
-        private void FormBalance_Load(object sender, EventArgs e)
+        private async void FormBalance_Load(object sender, EventArgs e)
         {
             label2.Text = id;
+            await CheckBalance();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -31,6 +34,43 @@ namespace ATM_Draft
             FormMain.ShowFormInPanel(frmMenu, mainForm.pnlChildForm);
 
 
+        }
+        private async Task CheckBalance()
+        {
+            try
+            {
+                Database database = new Database();
+                using (var connection = new SQLiteConnection(database.strConnection))
+                {
+                    await connection.OpenAsync();
+
+                    string query = "SELECT * FROM tblAccounts WHERE ID = @ID";
+
+                    using (var command = new SQLiteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@ID", id);
+                        using (var reader = await command.ExecuteReaderAsync())
+                        {
+                            if (reader.Read())
+                            {
+                                double current = Convert.ToDouble(reader["SAVINGS"]);
+                                double available = Convert.ToDouble(reader["SAVINGS"].ToString());
+
+                                lblCurrent.Text = "₱ " + string.Format("{0:0.00}", current);
+                                lblAvailable.Text = "₱ " + string.Format("{0:0.00}", available);
+
+
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            
         }
 
         
